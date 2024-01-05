ae4e8a43db362afc55a7080897a000d66e4b754a
unknown <alaa.lakouader@gmail.com>
2024-01-05 03:47:55 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Omni-Subsmissions</title>
+        <title>Omni-Submissions</title>
         <meta name="description" content="Casting powered By AI*" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
